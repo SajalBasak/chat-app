718a118452f34a3ce5439ef8e75de1d2ea5255ea
Sajal Basak <sajal.basak007@gmail.com>
2023-10-09 12:23:33 +0600

    git init

---
+++ router/usersRouter.js
@@ -0,0 +1,27 @@
+//external imports
+const express = require("express");
+
+//internal imports
+const { getUsers, addUser } = require("../controller/usersController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
+
+const router = express.Router();
+
+//users page
+router.get("/", decorateHtmlResponse("Users Section"), getUsers);
+
+//add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
+
+module.exports = router;
